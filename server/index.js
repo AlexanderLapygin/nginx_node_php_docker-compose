c869d9bbc632f5f39bfdd1e45ce09cdfb1e1fa81
AlexanderLapygin <alexanderlapygin@gmail.com>
2021-12-07 03:24:08 +0300

    refactor: removal of excess

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ const server = express();
 server.get('/', (req, res, next) => {
     res.json({ message: 'Hello world!' });
 });
+server.get('/endpoint1', (req, res, next) => {
+    res.json({ message: 'endpoint1: Hello world!' });
+});
+server.get('/endpoint2', (req, res, next) => {
+    res.json({ message: 'endpoint2 Hello world!' });
+});
 
 server.listen(8000, () => {
     console.log('Express server listening on 8000');
